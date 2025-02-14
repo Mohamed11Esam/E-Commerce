fc637db6335ed62922d0eb945048d23504b11cdd
Mohamed Esam <111438267+Mohamed11Esam@users.noreply.github.com>
2025-02-14 04:37:53 +0200

    handle NotFound Page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,8 +1,9 @@
 import styles from './NotFound.module.css'
+import noProducts from '../../assets/images/error.084f5f5b0eb10d3216cb7c939108e1f6.svg'
 function NotFound() {
     return (
-        <div>
-            NotFound
+        <div className='flex justify-center items-center min-h-dvh'>
+            <img src={noProducts}className='w-3/4' />
         </div>
     )
 }
